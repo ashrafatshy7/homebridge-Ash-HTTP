ece30c82130c7fb4bf47b3526b9e3cd098ba8f5d
ashrafatshy7 <ashrafatshy7@gmail.com>
2024-11-08 10:53:47 +0200

    aaaaaaaa

--- dist/settings.js
+++ dist/settings.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PLUGIN_NAME = exports.PLATFORM_NAME = void 0;
-exports.PLATFORM_NAME = 'HttpSensorsAndSwitches';
+exports.PLATFORM_NAME = 'http-ashraf';
 exports.PLUGIN_NAME = 'homebridge-http-sensors-switches';
--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,2 @@
-export const PLATFORM_NAME = 'HttpSensorsAndSwitches';
+export const PLATFORM_NAME = 'http-ashraf';
 export const PLUGIN_NAME = 'homebridge-http-sensors-switches';
